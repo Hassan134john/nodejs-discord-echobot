6e2da9e5fb6dc6025b897e800a1aecb0f9e3e2e4
Hassan134john <158227807+Hassan134john@users.noreply.github.com>
2024-01-31 11:05:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const Discord = require('discord.js'); //import client from discord
+const fs = require('fs');
+const {prefix} = require('./config.json');
+const Discord = require('discord.js'); //import discord.js
+const keep_alive = require('./keep_alive.js')
 
-const client = new Discord.Client();
+const client = new Discord.Client(); //create new client
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    // check if message isn't from us
-    if (msg.author == client.user) {
-      return;
-    }
-    else if (msg.content === 'ping') {
-      msg.reply('Pong!');
-    }
-    else {
-        msg.reply(msg.content);
-    }
-});
 
-client.login(process.env.TOKEN); //login bot using token
+
+//make sure this line is the last line
+client.login(process.env.token); //login bot using token
